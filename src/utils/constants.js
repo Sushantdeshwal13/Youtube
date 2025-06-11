abbdcf4e2e41d6cec5bbe998d13eb97b74b2455b
sushant <sushantdeshwal13@gmail.com>
2025-06-11 09:03:03 +0530

    refactor: moved api key to .env for security

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,16 +1,15 @@
-const GOOGLE_API_KEY = "AIzaSyBSok_cf0UQcXsAaZWRPC_UGVhC0RwEkXs";
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 export const YOUTUBE_VIDEO_API = 
   `https://youtube.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&chart=mostPopular&maxResults=50&regionCode=IN&key=${GOOGLE_API_KEY}`;
 
 export const LIVE_CHAT_COUNT = 10;
 
+// You can enable this when you need it
 // export const LIVE_CHAT_API = (videoId) =>
 //   `https://youtube.googleapis.com/youtube/v3/liveChat/messages?liveChatId=${videoId}&part=snippet,authorDetails&key=${GOOGLE_API_KEY}`;
 
- export const YOUTUBE_SEARCH_API = (query) =>
+export const YOUTUBE_SEARCH_API = (query) =>
   `https://api.allorigins.win/raw?url=${encodeURIComponent(
     "https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=" + query
   )}`;
-
-  
